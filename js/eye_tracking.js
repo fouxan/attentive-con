5a89c22dcb00d7fe67b5cdcc9a6c64d3ea1065c8
fouxan1310 <fouzanfouzan1310@gmail.com>
2023-09-06 00:00:53 +0530

    finalized conference part

--- js/eye_tracking.js
+++ js/eye_tracking.js
@@ -1,3 +1,73 @@
-webgazer.setListener((data, timestamp) => {
-    
-})
+window.saveDataAcrossSessions = true
+
+const THRESHOLD_TIME = 1000 // 1 second
+const LEFT_CUTOFF = window.innerWidth / 4
+const RIGHT_CUTOFF = window.innerWidth - window.innerWidth / 4
+
+
+
+let startLookTime = Number.POSITIVE_INFINITY
+let lookDirection = null
+
+webgazer.setGazeListener((data, timestamp) => {
+    if (data == null || lookDirection === "STOP") return
+    startLookTime = timestamp
+    if(startLookTime + THRESHOLD_TIME < timestamp){
+        videoElement = document.elementFromPoint(data.x, data.y)
+        console.log(videoElement)
+        shiftFocus('none')
+    }
+
+    // if (
+    //   data.x < LEFT_CUTOFF &&
+    //   lookDirection !== "LEFT" &&
+    //   lookDirection !== "RESET"
+    // ) {
+    //   startLookTime = timestamp
+    //   lookDirection = "LEFT"
+    // } else if (
+    //   data.x > RIGHT_CUTOFF &&
+    //   lookDirection !== "RIGHT" &&
+    //   lookDirection !== "RESET"
+    // ) {
+    //   startLookTime = timestamp
+    //   lookDirection = "RIGHT"
+    // } else if (data.x >= LEFT_CUTOFF && data.x <= RIGHT_CUTOFF) {
+    //   startLookTime = Number.POSITIVE_INFINITY
+    //   lookDirection = null
+    // }
+
+    // if (startLookTime + LOOK_DELAY < timestamp) {
+    //   if (lookDirection === "LEFT") {
+    //     imageElement.classList.add("left")
+    //   } else {
+    //     imageElement.classList.add("right")
+    //   }
+
+    //   startLookTime = Number.POSITIVE_INFINITY
+    //   lookDirection = "STOP"
+    //   setTimeout(() => {
+    //     imageElement.remove()
+    //     nextImageElement.classList.remove("next")
+    //     imageElement = nextImageElement
+    //     nextImageElement = getNewImage(true)
+    //     lookDirection = "RESET"
+    //   }, 200)
+    // }
+}).begin()
+
+function shiftFocus(user) {
+  if(user === 'none'){
+    console.log("changing focus to only host.")
+  }else if(user === 'one'){
+    console.log("changing focus to host and one")
+  }else if(user === 'two'){
+    console.log("changing focus to host and two")
+  }else if(user === 'three'){
+    console.log("changing focus to host and three")
+  }else if(user === 'four'){
+    console.log("changing focus to host and four")
+  }else{
+    console.log("host looked away")
+  }
+}
--- js/room_rtc.js
+++ js/room_rtc.js
@@ -28,9 +28,6 @@ if(!displayName){
 let localTracks = []
 let remoteUsers = {}
 
-let localScreenTracks;
-let sharingScreen = false;
-
 let joinRoomInit = async () => {
     rtmClient = await AgoraRTM.createInstance(APP_ID)
     await rtmClient.login({uid,token})
@@ -60,13 +57,11 @@ let joinStream = async () => {
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
-
     let player = `<div class="video__container" id="user-container-${uid}">
                     <div class="video-player" id="user-${uid}"></div>
                 </div>`
 
     document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
-    document.getElementById(`user-container-${uid}`).addEventListener('click', expandVideoFrame)
 
     localTracks[1].play(`user-${uid}`)
     await client.publish([localTracks[0], localTracks[1]])
@@ -82,7 +77,6 @@ let switchToCamera = async () => {
     await localTracks[1].setMuted(true)
 
     document.getElementById('mic-btn').classList.remove('active')
-    document.getElementById('screen-btn').classList.remove('active')
 
     localTracks[1].play(`user-${uid}`)
     await client.publish([localTracks[1]])
@@ -100,14 +94,6 @@ let handleUserPublished = async (user, mediaType) => {
             </div>`
 
         document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
-        document.getElementById(`user-container-${user.uid}`).addEventListener('click', expandVideoFrame)
-
-    }
-
-    if(displayFrame.style.display){
-        let videoFrame = document.getElementById(`user-container-${user.uid}`)
-        videoFrame.style.height = '100px'
-        videoFrame.style.width = '100px'
     }
 
     if(mediaType === 'video'){
@@ -125,17 +111,6 @@ let handleUserLeft = async (user) => {
     let item = document.getElementById(`user-container-${user.uid}`)
     if(item){
         item.remove()
-    }
-
-    if(userIdInDisplayFrame === `user-container-${user.uid}`){
-        displayFrame.style.display = null
-
-        let videoFrames = document.getElementsByClassName('video__container')
-
-        for(let i = 0; videoFrames.length > i; i++){
-            videoFrames[i].style.height = '300px'
-            videoFrames[i].style.width = '300px'
-        }
     }
 }
 
@@ -163,54 +138,6 @@ let toggleCamera = async (e) => {
     }
 }
 
-let toggleScreen = async (e) => {
-    let screenButton = e.currentTarget
-    let cameraButton = document.getElementById('camera-btn')
-
-    if(!sharingScreen){
-        sharingScreen = true
-
-        screenButton.classList.add('active')
-        cameraButton.classList.remove('active')
-        cameraButton.style.display = 'none'
-
-        localScreenTracks = await AgoraRTC.createScreenVideoTrack()
-
-        document.getElementById(`user-container-${uid}`).remove()
-        displayFrame.style.display = 'block'
-
-        let player = `<div class="video__container" id="user-container-${uid}">
-                <div class="video-player" id="user-${uid}"></div>
-            </div>`
-
-        displayFrame.insertAdjacentHTML('beforeend', player)
-        document.getElementById(`user-container-${uid}`).addEventListener('click', expandVideoFrame)
-
-        userIdInDisplayFrame = `user-container-${uid}`
-        localScreenTracks.play(`user-${uid}`)
-
-        await client.unpublish([localTracks[1]])
-        await client.publish([localScreenTracks])
-
-        let videoFrames = document.getElementsByClassName('video__container')
-        for(let i = 0; videoFrames.length > i; i++){
-            if(videoFrames[i].id != userIdInDisplayFrame){
-                videoFrames[i].style.height = '100px'
-                videoFrames[i].style.width = '100px'
-            }
-        }
-
-
-    }else{
-        sharingScreen = false 
-        cameraButton.style.display = 'block'
-        document.getElementById(`user-container-${uid}`).remove()
-        await client.unpublish([localScreenTracks])
-
-        switchToCamera()
-    }
-}
-
 let leaveStream = async (e) => {
     e.preventDefault()
 
@@ -224,29 +151,14 @@ let leaveStream = async (e) => {
 
     await client.unpublish([localTracks[0], localTracks[1]])
 
-    if(localScreenTracks){
-        await client.unpublish([localScreenTracks])
-    }
-
     document.getElementById(`user-container-${uid}`).remove()
-
-    if(userIdInDisplayFrame === `user-container-${uid}`){
-        displayFrame.style.display = null
-
-        for(let i = 0; videoFrames.length > i; i++){
-            videoFrames[i].style.height = '300px'
-            videoFrames[i].style.width = '300px'
-        }
-    }
 
     channel.sendMessage({text:JSON.stringify({'type':'user_left', 'uid':uid})})
 }
 
 
-
 document.getElementById('camera-btn').addEventListener('click', toggleCamera)
 document.getElementById('mic-btn').addEventListener('click', toggleMic)
-document.getElementById('screen-btn').addEventListener('click', toggleScreen)
 document.getElementById('join-btn').addEventListener('click', joinStream)
 document.getElementById('leave-btn').addEventListener('click', leaveStream)
 
