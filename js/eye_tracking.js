73a1515d3c49545e1b91e7ea324f3f487c3d5a14
fouxan <fouzanfouzan1310@gmail.com>
2023-11-14 03:20:06 +0530

    v2

--- js/eye_tracking.js
+++ js/eye_tracking.js
@@ -1,108 +1,108 @@
-const HOST_UID = "1";
+let userId = sessionStorage.getItem("uid");
+let hasJoined = sessionStorage.getItem("has_joined");
 const THRESHOLD_TIME = 1000;
 const RESET_DELAY = 500;
 const COOLDOWN = 10000;
-let isHost = sessionStorage.getItem("is_host") == 'true';
 let gazeStartTime = null;
 let resetGazeTimeout = null;
 let lastSwitchTime = null;
 let currentFocusedGroup = null;
 
 function isLookingAtElement(gazeData, element) {
-    if(typeof element === 'object' && element !== null && 'getBoundingClientRect' in element){
-        const elementRect = element.getBoundingClientRect();
-        const gazeX = gazeData.x;
-        const gazeY = gazeData.y;
+  if (element.id === `user-container-${userId}`) return false;
+  if (
+    typeof element === "object" &&
+    element !== null &&
+    "getBoundingClientRect" in element
+  ) {
+    const elementRect = element.getBoundingClientRect();
+    const gazeX = gazeData.x;
+    const gazeY = gazeData.y;
 
-        return (looking = gazeX >= elementRect.left - 100 &&
-        gazeX <= elementRect.right + 100 &&
-        gazeY >= elementRect.top - 100 &&
-        gazeY <= elementRect.bottom + 100);
-    }
-    return false;
+    return (looking =
+      gazeX >= elementRect.left - 100 &&
+      gazeX <= elementRect.right + 100 &&
+      gazeY >= elementRect.top - 100 &&
+      gazeY <= elementRect.bottom + 100);
+  }
+  return false;
 }
 
-let changeVolumeForUser = async (usersUID, volumeLevel) => {
-    if(usersUID === uid){
-        await localTracks[0].setVolume(volumeLevel);
-    }else{
-        let userObject = remoteUsers[usersUID];
-        await userObject.audioTrack.setVolume(volumeLevel);
-    }
+async function focusOnUser(uid) {
+  console.log("Focusing on user: ", uid);
+
+  await channel.sendMessage({
+    text: JSON.stringify({
+      type: "focus",
+      from: userId,
+      to: uid,
+    }),
+  });
 }
 
-function focusOnUser(uid) {
-    console.log('Focusing on user:', uid);
+function startEyeTracking() {
+  webgazer
+    .setGazeListener(function (gazeData) {
+      if (gazeData == null) {
+        gazeStartTime = null;
+        return;
+      }
+      let currentTime = new Date().getTime();
+      if (lastSwitchTime && currentTime - lastSwitchTime < COOLDOWN) return;
 
-    const focusedUserContainer = document.getElementById(`user-container-${uid}`);
-    if (focusedUserContainer) {
-        focusedUserContainer.classList.add('focused-user');
-        focusedUserContainer.classList.remove('unfocused-user');
-        changeVolumeForUser(uid, 100);
-        currentFocusedGroup = userGroups[uid];
-    }
-
-    for (let userId in remoteUsers) {
-        let currentUsersGroup = userGroups[userId];
-        console.log('Processing user:', userId);
-        if (userId !== uid && userId !== HOST_UID && currentUsersGroup !== currentFocusedGroup) {
-            const userContainer = document.getElementById(`user-container-${userId}`);
-            if (userContainer) {
-                userContainer.classList.add('unfocused-user');
-                userContainer.classList.remove('focused-user');
-                changeVolumeForUser(userContainer.id.split("-")[2], 20);
+      let videoContainers = document.getElementsByClassName("video__container");
+      for (container of videoContainers) {
+        if (isLookingAtElement(gazeData, container)) {
+          if (gazeStartTime === null) {
+            gazeStartTime = new Date().getTime();
+          } else {
+            const gazeDuration = new Date().getTime() - gazeStartTime;
+            if (gazeDuration >= THRESHOLD_TIME) {
+              let focusId = container.id.split("-")[2];
+              console.log(`User ${userId} is looking at ${container.id}`);
+              if (
+                !container.classList.contains("focused-user") &&
+                userId !== focusId
+              ) {
+                console.log(users);
+                let focusName = users[focusId].name;
+                let userWantsToFocus = confirm(
+                  `Do you want to switch focus to ${focusName}?`
+                );
+                if (userWantsToFocus) {
+                  focusOnUser(focusId);
+                }
+                gazeStartTime = null;
+                lastSwitchTime = currentTime;
+              }
             }
+          }
+
+          if (resetGazeTimeout) {
+            clearTimeout(resetGazeTimeout);
+            resetGazeTimeout = null;
+          }
+        } else {
+          if (!resetGazeTimeout) {
+            resetGazeTimeout = setTimeout(() => {
+              gazeStartTime = null;
+              resetGazeTimeout = null;
+            }, RESET_DELAY);
+          }
         }
-    }
+      }
+    })
+    .begin();
+
+  webgazer.showVideoPreview(false).showPredictionPoints(true);
 }
 
-
-if(isHost){
-    webgazer.setGazeListener(function (gazeData, elapsedTime) {
-        if (gazeData == null) {
-            gazeStartTime = null;
-            return;
-        }
-        let currentTime = new Date().getTime();
-        if(lastSwitchTime && currentTime - lastSwitchTime < COOLDOWN) return;
-
-        let videoContainers = document.getElementsByClassName("video__container");
-        for(container of videoContainers){
-            if (isLookingAtElement(gazeData, container)) {
-                if (gazeStartTime === null) {
-                    gazeStartTime = new Date().getTime();
-                }else {
-                    const gazeDuration = new Date().getTime() - gazeStartTime;
-                    if (gazeDuration >= THRESHOLD_TIME) {
-                        if (!container.classList.contains("focused-user") && container.id != `user-container-${HOST_UID}`){
-                            let hostWantsToFocus = confirm("Do you want to switch focus?")
-                            if (hostWantsToFocus) {
-                                focusOnUser(container.id.split("-")[2])
-                            }
-                            gazeStartTime = null;
-                            lastSwitchTime = currentTime;
-                        }
-                    }
-                }
-
-                if (resetGazeTimeout) {
-                    clearTimeout(resetGazeTimeout);
-                    resetGazeTimeout = null;
-
-                }
-            } else {
-                if (!resetGazeTimeout) {
-                    resetGazeTimeout = setTimeout(() => {
-                        gazeStartTime = null;
-                        resetGazeTimeout = null;
-                    }, RESET_DELAY);
-                }
-            }
-
-        }
-    }).begin();
-
-    webgazer.showVideoPreview(false).showPredictionPoints(false);
-}
-
-
+let checkJoinedInterval = setInterval(() => {
+  hasJoined = sessionStorage.getItem("has_joined");
+  userId = sessionStorage.getItem("uid");
+  if (userId && hasJoined && hasJoined === "true") {
+    clearInterval(checkJoinedInterval);
+    console.log("User has joined");
+    startEyeTracking();
+  }
+}, 1000);
--- js/room_rtc.js
+++ js/room_rtc.js
@@ -1,4 +1,4 @@
-const APP_ID = "af633987e6be495aafe799b3d6daca51"
+const APP_ID = "af633987e6be495aafe799b3d6daca51";
 
 let token = null;
 let client;
@@ -6,213 +6,342 @@ let client;
 let rtmClient;
 let channel;
 
-let uid = sessionStorage.getItem('uid')
-if(!uid){
-    uid = String(Math.floor(Math.random() * 10000))
-    sessionStorage.setItem('uid', uid)
+let uid = sessionStorage.getItem("uid");
+if (!uid) {
+  uid = String(Math.floor(Math.random() * 10000));
+  sessionStorage.setItem("uid", uid);
 }
-const hostUID = "1";
-isHost = sessionStorage.getItem("is_host") == 'true';
-if(isHost){
-    uid = hostUID;
-    sessionStorage.setItem("uid", hostUID)
+const isHost = sessionStorage.getItem("is_host") == "true";
+if (isHost) {
+  console.log(`I am the host ${isHost}`);
 }
 
-let userGroups = {
-    "1": "g0",
+let userName = sessionStorage.getItem("display_name");
+if (!userName) {
+  window.location = "index.html";
+}
+
+let groups = {};
+let users = {
+  [uid]: {
+    id: uid,
+    name: userName,
+  },
 };
-const groupSize = 2;
-let numberOfParticipants = 0;
 
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString)
-let roomId = urlParams.get('room')
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get("room");
 
-if(!roomId){
-    roomId = 'main'
+if (!roomId) {
+  roomId = "main";
 }
 
-let displayName = sessionStorage.getItem('display_name')
-if(!displayName){
-    window.location = 'index.html'
-}
-
-let localTracks = []
-let remoteUsers = {}
+let localTracks = [];
+let remoteUsers = {};
 
 let joinRoomInit = async () => {
-    rtmClient = await AgoraRTM.createInstance(APP_ID)
-    await rtmClient.login({uid,token})
+  rtmClient = await AgoraRTM.createInstance(APP_ID);
+  await rtmClient.login({ uid, token });
 
-    await rtmClient.addOrUpdateLocalUserAttributes({'name':displayName})
+  await rtmClient.addOrUpdateLocalUserAttributes({ name: userName });
 
-    channel = await rtmClient.createChannel(roomId)
-    await channel.join()
+  channel = await rtmClient.createChannel(roomId);
+  await channel.join();
 
-    channel.on('ChannelMessage', handleChannelMessage)
+  client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
-    client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
-    await client.join(APP_ID, roomId, token, uid)
+  channel.on("ChannelMessage", handleChannelMessage);
 
-    client.on('user-published', handleUserPublished)
-    client.on('user-left', handleUserLeft)
-}
+  await client.join(APP_ID, roomId, token, uid);
+
+  client.on("user-published", handleUserPublished);
+  client.on("user-left", handleUserLeft);
+};
 
 let joinStream = async () => {
-    document.getElementById('join-btn').style.display = 'none'
-    document.getElementsByClassName('stream__actions')[0].style.display = 'flex'
+  sessionStorage.setItem("has_joined", true);
+  console.log("User has joined, setting has_joined to true");
+  document.getElementById("join-btn").style.display = "none";
+  document.getElementsByClassName("stream__actions")[0].style.display = "flex";
 
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
 
-    let player = `<div class="video__container" id="user-container-${uid}">
+  let player = `<div class="video__container" id="user-container-${uid}">
                     <div class="video-player" id="user-${uid}"></div>
-                </div>`
+                </div>`;
 
-    document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
+  document
+    .getElementById("streams__container")
+    .insertAdjacentHTML("beforeend", player);
 
-    localTracks[1].play(`user-${uid}`)
-    await client.publish([localTracks[0], localTracks[1]])
-
-    if(isHost){
-        videoContainer = document.getElementById(`user-container-${hostUID}`)
-        videoContainer.classList.add("focused-user")
-    }
-}
+  localTracks[1].play(`user-${uid}`);
+  await client.publish([localTracks[0], localTracks[1]]);
+  const videoContainer = document.getElementById(`user-container-${uid}`);
+  videoContainer.classList.add("me");
+  const nameLabel = document.createElement("div");
+  nameLabel.classList.add("name-label");
+  nameLabel.textContent = users[uid].name;
+  videoContainer.appendChild(nameLabel);
+  console.log(videoContainer);
+  channel.sendMessage({
+    text: JSON.stringify({ type: "user_joined", uid: uid, name: userName }),
+  });
+  console.log("User has joined, sending user_joined message");
+};
 
 let handleUserPublished = async (user, mediaType) => {
-    remoteUsers[user.uid] = user
-    await client.subscribe(user, mediaType)
+  remoteUsers[user.uid] = user;
+  let { name } = await rtmClient.getUserAttributesByKeys(user.uid, ["name"]);
+  console.log(name);
+  await client.subscribe(user, mediaType);
 
-    let player = document.getElementById(`user-container-${user.uid}`)
-    if(player === null){
-        player = `<div class="video__container" id="user-container-${user.uid}">
+  let player = document.getElementById(`user-container-${user.uid}`);
+  if (player === null) {
+    player = `<div class="video__container" id="user-container-${user.uid}">
                 <div class="video-player" id="user-${user.uid}"></div>
-            </div>`
+            </div>`;
 
-        document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
-    }
+    document
+      .getElementById("streams__container")
+      .insertAdjacentHTML("beforeend", player);
+  }
 
-    if(mediaType === 'video'){
-        user.videoTrack.play(`user-${user.uid}`)
-    }
+  if (mediaType === "video") {
+    user.videoTrack.play(`user-${user.uid}`);
+    const videoContainer = document.getElementById(
+      `user-container-${user.uid}`
+    );
+    const nameLabel = document.createElement("div");
+    nameLabel.classList.add("name-label");
+    nameLabel.textContent = name;
+    videoContainer.appendChild(nameLabel);
+  }
 
-    if(mediaType === 'audio'){
-        user.audioTrack.play()
-    }
-
-    if(isHost){
-        // videoContainer = document.getElementById(`user-container-${hostUID}`)
-        // videoContainer.classList.add("focused-user")
-        assignGroup(user.uid);
-    }
-}
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+  updateVolumeAndBorderColor();
+};
 
 let handleUserLeft = async (user) => {
-    if(user.uid === hostUID){
-        channel.sendMessage({text:JSON.stringify({'type':'end_meeting'})});
-    }
-    delete remoteUsers[user.uid]
-    let item = document.getElementById(`user-container-${user.uid}`)
-    if(item){
-        item.remove()
-    }
-}
+  delete remoteUsers[user.uid];
+  let item = document.getElementById(`user-container-${user.uid}`);
+  if (item) {
+    item.remove();
+  }
+  delete users[user.uid];
+};
 
 let toggleMic = async (e) => {
-    let button = e.currentTarget
+  let button = e.currentTarget;
 
-    if(localTracks[0].muted){
-        await localTracks[0].setMuted(false)
-        button.classList.add('active')
-    }else{
-        await localTracks[0].setMuted(true)
-        button.classList.remove('active')
-    }
-}
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false);
+    button.classList.add("active");
+  } else {
+    await localTracks[0].setMuted(true);
+    button.classList.remove("active");
+  }
+};
 
 let toggleCamera = async (e) => {
-    let button = e.currentTarget
+  let button = e.currentTarget;
 
-    if(localTracks[1].muted){
-        await localTracks[1].setMuted(false)
-        button.classList.add('active')
-    }else{
-        await localTracks[1].setMuted(true)
-        button.classList.remove('active')
-    }
-}
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false);
+    button.classList.add("active");
+  } else {
+    await localTracks[1].setMuted(true);
+    button.classList.remove("active");
+  }
+};
 
 let leaveStream = async (e) => {
-    if (e && typeof e.preventDefault === 'function') {
-        e.preventDefault();
+  if (e && typeof e.preventDefault === "function") {
+    e.preventDefault();
+  }
+
+  document.getElementById("join-btn").style.display = "block";
+  document.getElementsByClassName("stream__actions")[0].style.display = "none";
+
+  for (let i = 0; localTracks.length > i; i++) {
+    localTracks[i].stop();
+    localTracks[i].close();
+  }
+
+  await client.unpublish([localTracks[0], localTracks[1]]);
+
+  document.getElementById(`user-container-${uid}`).remove();
+
+  channel.sendMessage({
+    text: JSON.stringify({ type: "user_left", uid: uid }),
+  });
+  if (isHost) {
+    channel.sendMessage({ text: JSON.stringify({ type: "end_meeting" }) });
+  }
+};
+
+function updateVolumeAndBorderColor() {
+  for (let userId in users) {
+    let videoContainer = document.getElementById(`user-container-${userId}`);
+    if (videoContainer) {
+      if (groups[userId] && groups[userId].includes(uid)) {
+        setBorderColor("green", userId);
+        setVolume(100, userId);
+      } else {
+        setBorderColor("red", userId);
+        setVolume(20, userId);
+      }
     }
-
-    document.getElementById('join-btn').style.display = 'block'
-    document.getElementsByClassName('stream__actions')[0].style.display = 'none'
-
-    for(let i = 0; localTracks.length > i; i++){
-        localTracks[i].stop()
-        localTracks[i].close()
-    }
-
-    await client.unpublish([localTracks[0], localTracks[1]])
-
-    document.getElementById(`user-container-${uid}`).remove()
-
-    channel.sendMessage({text:JSON.stringify({'type':'user_left', 'uid':uid})})
-    if(uid === hostUID){
-        channel.sendMessage({text:JSON.stringify({"type":'end_meeting'})})
-    }
+  }
 }
 
-let handleChannelMessage = async (messageData, MemberId)=>{
-    let data = JSON.parse(messageData.text)
-    console.log(`A new message was received ${data.type}`)
-    if(data.type === 'user_left'){
-        document.getElementById(`user-container-${data.uid}`).remove()
-        delete userGroups[data.uid]
-    }
+let handleChannelMessage = async (messageData) => {
+  let data = JSON.parse(messageData.text);
+  console.log(`New message: ${data.type}`);
+  console.log(data);
 
-    if(data.type === 'end_meeting'){
-        alert("The host has ended the meeting.");
-        leaveStream();
-    }
+  if (data.type === "user_left") {
+    document.getElementById(`user-container-${data.uid}`).remove();
+    delete groups[data.uid];
+  }
 
-    if(data.type === 'group_assignment'){
-        userGroups[data.uid] = data.group;
-        sessionStorage.setItem(`group_of_${data.uid}`, data.group)
+  if (data.type === "user_joined") {
+    users[data.uid] = {
+      id: data.uid,
+      name: data.name,
+    };
+    console.log(users);
+
+    await channel.sendMessage({
+      text: JSON.stringify({
+        type: "user_list",
+        users: users,
+        groups: groups,
+        to: data.uid,
+      }),
+    });
+    updateVolumeAndBorderColor();
+  }
+
+  if (data.type === "user_list" && data.to === uid) {
+    users = data.users;
+    groups = data.groups;
+    console.log(users);
+    console.log(groups);
+    updateVolumeAndBorderColor();
+  }
+
+  if (data.type === "end_meeting") {
+    confirm("The host has ended the meeting. Redirecting to Lobby...");
+    leaveStream();
+    leaveChannel();
+    window.location = "index.html";
+  }
+
+  if (data.type === "focus" && data.to === uid) {
+    console.log(`User ${data.to} is focusing on ${data.from}.`);
+    if (data.to === uid) {
+      let promptingName = users[data.from].name;
+      alert(`${promptingName} is focusing on you.`);
+
+      let focusedUserGroup = groups[data.to];
+
+      if(!focusedUserGroup) {
+        groups[data.to] = [data.to, data.from];
+        groups[data.from] = [data.to, data.from];
+      } else {
+
+        let focusingUserGroup = groups[data.from];
+        if(focusingUserGroup) {
+          let index = focusingUserGroup.indexOf(data.from);
+          if (index !== -1) {
+            focusingUserGroup.splice(index, 1);
+          }
+        }
+
+        focusedUserGroup.push(data.from);
+
+        for (let userId of focusedUserGroup){
+          groups[userId] = [...focusedUserGroup];
+        }
+      }
+
+      for (let userId in users) {
+        await channel.sendMessage({
+          text: JSON.stringify({
+            type: "group_update",
+            group: groups[data.from],
+          }),
+        });
+      }
+      console.log(groups);
+      updateVolumeAndBorderColor();
     }
-}
+  }
+
+  if (data.type === "group_update") {
+    // Update the group information for the users in the group
+    for (let userId of data.group) {
+      groups[userId] = data.group;
+    }
+    console.log(groups);
+    updateVolumeAndBorderColor();
+  }
+
+  // if (data.type === "ignore" && data.to === uid) {
+  //   // Notify the user that they were ignored
+  //   if (data.to === uid) {
+  //     let promptedName = users[data.from].name;
+  //     alert(`${promptedName} ignored your focus.`);
+  //   }
+  // }
+};
 
 let leaveChannel = async () => {
-    await channel.leave()
-    await rtmClient.logout()
+  await channel.leave();
+  await rtmClient.logout();
+};
+
+// async function promptUser(userId, message) {
+//   // Check if the current user is the one being prompted
+//   if (userId === uid) {
+//     return Promise.resolve(confirm(message));
+//   } else {
+//     // If the current user is not the one being prompted, return false
+//     return Promise.resolve(false);
+//   }
+// }
+
+function setVolume(volumeLevel, userID) {
+  if (userID === uid) {
+    localTracks[0].setVolume(volumeLevel);
+  } else {
+    let userObject = remoteUsers[userID];
+    userObject.audioTrack.setVolume(volumeLevel);
+  }
 }
 
-function hostExists(){
-    return document.getElementById(`user-container-${hostUID}`) !== null;
+function setBorderColor(color, userID) {
+  let videoContainer = document.getElementById(`user-container-${userID}`);
+  if (videoContainer) {
+    if (color === "green") {
+      videoContainer.classList.add("focused-user");
+      videoContainer.classList.remove("unfocused-user");
+    } else {
+      videoContainer.classList.add("unfocused-user");
+      videoContainer.classList.remove("focused-user");
+    }
+  }
 }
 
-function assignGroup(userID){
-    if(userID === hostUID) return;
+(async () => {
+  await joinRoomInit();
+})();
 
-    let otherUsers = Array.from(document.getElementsByClassName("video__container")).filter(elem => !elem.id.includes(hostUID));
-    numberOfParticipants = otherUsers.length + 1;
-    console.log(`otherUsers: ${otherUsers}`);
-    console.log(`totalParticipants: ${numberOfParticipants}`);
-
-    const assignedGroup = `g${Math.ceil((numberOfParticipants)/groupSize)}`
-    userGroups[userID] = assignedGroup
-    sessionStorage.setItem(`group_of_${userID}`, assignedGroup)
-    channel.sendMessage({
-        text: JSON.stringify({'type': 'group_assignment', 'uid': userID, 'group': assignedGroup}),
-    });
-}
-
-document.getElementById('camera-btn').addEventListener('click', toggleCamera)
-document.getElementById('mic-btn').addEventListener('click', toggleMic)
-document.getElementById('join-btn').addEventListener('click', joinStream)
-document.getElementById('leave-btn').addEventListener('click', leaveStream)
-window.addEventListener('beforeunload', leaveChannel)
-
-joinRoomInit()
+document.getElementById("join-btn").addEventListener("click", joinStream);
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("leave-btn").addEventListener("click", leaveStream);
+window.addEventListener("beforeunload", leaveChannel);
